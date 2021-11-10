3285fc0731076bd4385cd25c4a95dc5e54f9c9c9
trigaten <sanderman5@hotmail.com>
2021-11-10 15:34:22 -0500

    init files

---
+++ env/pvp_env.py
@@ -0,0 +1,2 @@
+"""Eventually we should be able to have our entire environment code here. We shouldnt need it to sit within
+the minerl code. """
---
+++ training/model.py
@@ -0,0 +1,2 @@
+"""Contains the pytorch neural network"""
+import torch

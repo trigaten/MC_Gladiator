99ed43498b0c7824baec2418aff80b8f03b34def
trigaten <sanderman5@hotmail.com>
2021-11-23 10:10:41 -0500

    for testing

--- training/main.py
+++ training/main.py
@@ -1,1 +1,8 @@
 """Contains the training loop/routine"""
+
+import sys
+sys.path.append("..")
+# from env.pvpbox_specs import PvpBox
+
+from env.multi_pvp_box import PvpBoxMultiAgent
+# load the env

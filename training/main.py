f22485c325420430787ea712045e6189c753f4ce
trigaten <sanderman5@hotmail.com>
2021-11-26 22:18:41 -0500

    pvp wrapper

--- training/main.py
+++
@@ -1,8 +0,0 @@
-"""Contains the training loop/routine"""
-
-import sys
-sys.path.append("..")
-# from env.pvpbox_specs import PvpBox
-
-from env.multi_pvp_box import PvpBoxMultiAgent
-# load the env
--- training/model.py
+++ training/model.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class gnet(nn.Module):
+class PPO_net(nn.Module):
   def __init__(self):
         super().__init__()
 

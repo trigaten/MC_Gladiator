4ef3a20a37dde9a4bd13b80330a834dd8ccbbf16
trigaten <sanderman5@hotmail.com>
2021-12-29 14:52:48 -0500

    begin conversion to rllib

--- training/multidummy.py
+++ training/multidummy.py
@@ -12,6 +12,8 @@ from env.dummy_spec import Dummy
 from env.wrappers import OneVersusOneWrapper
 from env.wrappers import OpponentStepWrapper
 
+from ray.rllib.agents.ppo import PPOTrainer
+
 from model import Discrete_PPO_net
 from Agent import Agent
 
@@ -41,48 +43,5 @@ optimizer = optim.Adam(hero.net.parameters(), lr=LR)
 loss_func = torch.nn.CrossEntropyLoss()
 
 # training loop
-while True:
-    value_batch = []
-    action_batch = []
-    action_distribution_batch = []
-    adv_batch = []
-    # obtain batch
-    for i in range(BATCH_SIZE):
-        obs = env.reset()
-        values = []
-        actions_taken = []
-        action_distributions = []
-        rewards = []
-        steps = 0
 
-        done = False
-        while not done:
-            steps += 1
-            # get hero action
-            action, action_distribution, value = hero(obs)
-            # append items
-            actions_taken.append(action)
-            action_distributions.append(action_distribution)
-            values.append(value)
-
-            # step with action
-            obs, reward, done, info = env.step(action)
-            rewards.append(reward)
-            # print(reward)
-
-        value_batch.append(values)
-        action_batch.append(actions_taken)
-        action_distribution_batch.append(action_distributions)
-        adv_batch.append(GAE_adv(rewards, values, DISCOUNT, LAMBDA))
-
-    # train on batch
-    for i in range(EPOCHS):
-        action_batch = torch.FloatTensor(action_batch)
-        exit()
-
-
-    
-
-    
-
-
+# policy = ts.policy.PPOPolicy(actor, critic, optimizer, distribution, value_clip=False, advantage_normalization=False)

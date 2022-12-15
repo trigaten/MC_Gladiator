0ba9dfa5b6114be2209b2a319ed1ad604dac0cf1
trigaten <sanderschulhoff@gmail.com>
2022-12-15 18:14:02 -0500

    feat: p

--- src/MCGladiator/server.py
+++ src/MCGladiator/server.py
@@ -1,9 +1,9 @@
 class Server:
-    def __init__(fifo:str):
+    def __init__(self, fifo:str):
         self.fifo = fifo
 
     def start(self):
-        self.fp = open("urfifo", "w")
+        self.fp = open(self.fifo, "w")
 
     def execute(self, cmd:str):
         self.fp.write(cmd)

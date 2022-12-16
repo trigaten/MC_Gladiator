2c83b4fcd50004380ec5cfed28e5e2a934d75278
Trigaten <sanderschulhoff@gmail.com>
2022-12-16 14:00:52 -0500

    feat: thing

--- controller.py
+++ controller.py
@@ -1,7 +1,29 @@
-from mcgladiator.server import Server
+from MCGladiator.server import Server
 import subprocess
+from minerl.herobraine.env_specs.human_survival_specs import HumanSurvival
+from typing import List
+from minerl.herobraine.hero.handler import Handler
+import minerl.herobraine.hero.handlers as handlers
+import coloredlogs
+import logging
+coloredlogs.install(logging.DEBUG)
+from minerl.human_play_interface.human_play_interface import HumanPlayInterface
+
+from src.MCGladiator.flat_env import HumanSurvivalMultiplayer
 
 # spin up minecraft server
 
 
 server = Server("mc_fifo")
+
+env = HumanSurvivalMultiplayer("127.0.0.1:25565", "human").make()
+
+env = HumanPlayInterface(env)
+print(env.reset())
+
+done = False
+
+while not done:
+    obs, reward, done, _ = env.step()
+    print(obs["life_stats"])
+    server.execute("/give @a diamond")
